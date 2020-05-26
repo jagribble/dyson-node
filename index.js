8721d8e80acd54c651044d54c24a509068b1c6b2
Jules Gribble <gribblej@me.com>
2020-05-26 02:18:41 +0100

    export

--- index.js
+++ index.js
@@ -1,27 +1,4 @@
-const fetch = require('node-fetch');
+const DysonAccount = require('./DysonAccount');
+const DysonDevice = require('./DysonDevice');
 
-const DYSON_API_URL = 'appapi.cp.dyson.com';
-
-class DysonAccount{
-    constructor(email, password, country = 'GB'){
-        this.email = email;
-        this.password = password;
-        this.country = country;
-    }
-
-    login(){
-        fetch(`https://${DYSON_API_URL}/v1/userregistration/authenticate?country=${this.country}`, {
-            method: 'POST',
-            body: JSON.stringify({
-                Email: this.email,
-                Password: this.password,
-            })
-        }).then(account => {
-            console.log(account);
-        }).catch(err => {
-            console.error(err);
-        });
-    }
-}
-
-module.exports = DysonAccount;
+module.exports = { DysonAccount, DysonDevice };
